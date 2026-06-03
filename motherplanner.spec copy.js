4318db804ff2d3624619dfe4a8946ba110c01485
Your Name <your-email@example.com>
2026-06-03 18:27:08 +0530

    Updated Playwright automation scripts

--- motherplanner.spec copy.js
+++ motherplanner.spec copy.js
@@ -2,7 +2,7 @@ const { test, expect } = require('@playwright/test');
 const fs = require('fs');
 const path = require('path');
 
-test.setTimeout(1800000);
+test.setTimeout(1600000);
 
 test.use({
   ignoreHTTPSErrors: true,
